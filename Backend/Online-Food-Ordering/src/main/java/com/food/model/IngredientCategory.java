4c435da319d4243a039b4d2298c2e5d4826ea679
tox-in <habimanat78@gmail.com>
2024-04-26 15:56:45 +0200

    full IngredientCategory entity content

---
+++ Backend/Online-Food-Ordering/src/main/java/com/food/model/IngredientCategory.java
@@ -0,0 +1,30 @@
+package com.food.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.minidev.json.annotate.JsonIgnore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class IngredientCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<IngredientsItem> ingredients = new ArrayList<>();
+
+}
