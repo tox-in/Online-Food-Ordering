313e5a4e705e4f079e889b34ede059cf4e89e838
tox-in <habimanat78@gmail.com>
2024-04-26 10:49:41 +0200

    IngredientsItem entity full content

---
+++ src/main/java/com/food/model/IngredientsItem.java
@@ -0,0 +1,27 @@
+package com.food.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.minidev.json.annotate.JsonIgnore;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class IngredientsItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    @ManyToOne
+    private IngredientCategory category;
+
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+
+    private boolean inStoke = true;
+}
