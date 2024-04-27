d8cef58f76b78d06460a48a5555e004904f3f402
tox-in <habimanat78@gmail.com>
2024-04-27 16:30:57 +0200

    changes in CartItem entity model

---
+++ Backend/Online-Food-Ordering/src/main/java/com/food/model/CartItem.java
@@ -0,0 +1,34 @@
+package com.food.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.minidev.json.annotate.JsonIgnore;
+
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JsonIgnore
+    private Cart cart;
+
+    @ManyToOne
+    private Food food;
+
+    private int quantity;
+
+    private List<String> ingredients;
+
+    private Long totalPrice;
+
+
+}
