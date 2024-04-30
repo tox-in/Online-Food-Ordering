0b2809f18e93db56156bea534d4bf3718d0ee36a
tox-in <habimanat78@gmail.com>
2024-04-30 08:10:18 +0200

    jwt assignment and related logical actions in JwtProvider

--- Backend/Online-Food-Ordering/src/main/java/com/food/config/JwtProvider.java
+++ Backend/Online-Food-Ordering/src/main/java/com/food/config/JwtProvider.java
@@ -1,14 +1,39 @@
 package com.food.config;
 
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.util.*;
 
 @Service
 public class JwtProvider {
 
     SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
 
-    public String generateToken(2:39:12)
+    public String generateToken(Authentication auth){
+    Collection<? extends GrantedAuthority>authorities = auth.getAuthorities();
+    String roles = populateAuthorities(authorities);
+
+    String jwt = Jwts.builder().setIssuedAt(new Date())
+            .setExpiration(new Date(new Date().getTime()+86400000))
+            .claim("email", auth.getName())
+            .claim("authorities", roles)
+            .signWith(key)
+            .compact();
+
+    return jwt;
+    }
+
+    private String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        Set<String> auths = new HashSet<>();
+
+        for(GrantedAuthority authority:authorities){
+            auths.add(authority.getAuthority());
+        }
+        return String.join(",",auths);
+    }
 }
