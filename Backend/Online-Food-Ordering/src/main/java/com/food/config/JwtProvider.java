f93416a15c2db9267eafee1ba2620bc4a8403d4c
tox-in <habimanat78@gmail.com>
2024-04-30 08:18:35 +0200

    added getEmailFromJwtToken method in JwtProvider

--- Backend/Online-Food-Ordering/src/main/java/com/food/config/JwtProvider.java
+++ Backend/Online-Food-Ordering/src/main/java/com/food/config/JwtProvider.java
@@ -1,5 +1,6 @@
 package com.food.config;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
@@ -26,6 +27,15 @@ public class JwtProvider {
             .compact();
 
     return jwt;
+    }
+
+    public String getEmailFromJwtToken(String jwt){
+        jwt = jwt.substring(7);
+        Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+
+        String email = String.valueOf(claims.get("email"));
+
+        return email;
     }
 
     private String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
