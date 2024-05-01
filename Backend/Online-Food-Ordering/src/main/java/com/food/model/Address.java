c888d09f76a822f15ff9b26c8c12252bac0d5c78
tox-in <habimanat78@gmail.com>
2024-05-01 08:07:19 +0200

    changes in Address and IngredientsItem

--- Backend/Online-Food-Ordering/src/main/java/com/food/model/Address.java
+++ Backend/Online-Food-Ordering/src/main/java/com/food/model/Address.java
@@ -5,8 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.io.Serializable;
+
 @Entity
-public class Address {
+public class Address implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- Backend/Online-Food-Ordering/src/main/java/com/food/model/IngredientsItem.java
+++ Backend/Online-Food-Ordering/src/main/java/com/food/model/IngredientsItem.java
@@ -6,17 +6,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import net.minidev.json.annotate.JsonIgnore;
 
+import java.io.Serializable;
+
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class IngredientsItem {
+public class IngredientsItem implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
     @ManyToOne
+//    @JoinColumn(name = "id")
     private IngredientCategory category;
 
     @JsonIgnore
